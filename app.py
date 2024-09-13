0ac1d22e9d4daab65042f7007f05d074f8a603d8
Pedro Di Gianni <pedrodigianni@gmail.com>
2024-09-13 11:45:14 -0300

    app

--- app.py
+++ app.py
@@ -144,6 +144,8 @@ def season_dashboard():
 
     except IndexError:
         return "No matching data for the selected season.", 404
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
